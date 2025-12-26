fcbbfd1a739bfc68e83d2c49d9125622044f9245
yohans-kasaw <emailyohans@gmail.com>
2025-12-26 23:09:09 +0300

    user model and migration

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
+
+	"libam/db"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lmittmann/tint"
 )
 
 func main() {
+	logger := slog.New(tint.NewHandler(os.Stdout, &tint.Options{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+	}))
+
+	_, err := db.InitDB(os.Getenv("GOOSE_DBSTRING"))
+
+	if err != nil {
+		logger.Error("Error when connecting to db", "error", err)
+		os.Exit(1)
+	}
 
 	r := gin.Default()
-
 	r.GET("/health", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "healthy")
 	})
