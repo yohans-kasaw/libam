ab9890f90edd1884a08ee8ce12420cd9731cbedd
yohans-kasaw <emailyohans@gmail.com>
2026-01-13 14:12:28 +0300

    web, devtools: production dev tools of tanstack

---
+++ web/src/App.tsx
@@ -0,0 +1,46 @@
+import { lazy, Suspense, useEffect, useState } from 'react'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
+
+// adds type safty for <Links>
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+const router = createRouter({
+    routeTree,
+})
+
+const queryClient = new QueryClient()
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+    import('@tanstack/react-query-devtools/production').then(
+        (d) => ({ default: d.ReactQueryDevtools }),
+    ),
+)
+
+export default function App() {
+    const [showDevTools, setShowDevTools] = useState(false)
+
+    useEffect(() => {
+        // @ts-expect-error - no type for windows 
+        window.toggleDevTools = () => setShowDevTools((old) => !old)
+    }, [])
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools />
+            {showDevTools && (
+                <Suspense fallback={null}>
+                    <ReactQueryDevtoolsProduction />
+                </Suspense>
+            )}
+        </QueryClientProvider>
+    )
+}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,30 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-import './index.css'
-
-const router = createRouter({
-    routeTree,
-})
-
-const queryClient = new QueryClient()
-
-// adds type safty for Links
-declare module '@tanstack/react-router' {
-    interface Register {
-        router: typeof router
-    }
-}
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-            <ReactQueryDevtools />
-        </QueryClientProvider>
+        <App/>
     </StrictMode>,
 )
