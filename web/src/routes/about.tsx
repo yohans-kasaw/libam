ef026f323913ff9eeaa76c7b36137f9807e29c95
yohans-kasaw <emailyohans@gmail.com>
2026-01-13 20:18:08 +0300

    dummy json: query testing

--- web/src/routes/about.tsx
+++ web/src/routes/about.tsx
@@ -1,13 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import ky from "ky";
+
+interface Response {
+    status: string,
+    method: string,
+}
 
 export const Route = createFileRoute('/about')({
     component: About,
 })
 
 function About() {
+    const { data, isLoading } = useQuery({
+        queryKey: ["dummyJson"],
+        queryFn: (): Promise<Response> => {
+            return ky.get("https://dummyjson.com/test").json()
+        }
+    })
     return (
         <div>
             Hello from "About"
+            <br />
+            {isLoading && "loadig .."}
+            {
+                data?.status
+            }
+            <br />
+            {
+                data?.method
+            }
         </div>
     )
 
