4a776c24718df038b0254a64bd33f281dda920fd
yohans-kasaw <emailyohans@gmail.com>
2026-01-13 13:33:25 +0300

    react query: added devtools

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 
@@ -9,15 +11,20 @@ const router = createRouter({
     routeTree,
 })
 
+const queryClient = new QueryClient()
+
 // adds type safty for Links
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+    interface Register {
+        router: typeof router
+    }
 }
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </StrictMode>,
 )
--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,20 +1,18 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 function RootComponent() {
     return (
         <>
             <div>Hello "__root"!</div>
             <div>
-                <Link to="/">
-                    home
-                </Link>
+                <Link to="/">home</Link>
             </div>
             <div>
-                <Link to="/about">
-                    about
-                </Link>
+                <Link to="/about">about</Link>
             </div>
             <Outlet></Outlet>
+            <TanStackRouterDevtools />
         </>
     )
 }
