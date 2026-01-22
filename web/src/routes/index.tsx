ec556e3cc23e244d473b675c751dac4ca187407d
yohans-kasaw <emailyohans@gmail.com>
2026-01-22 20:55:30 +0300

    web: index redirect to home

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,7 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
     component: RouteComponent,
+
+    beforeLoad: () => {
+        throw redirect({ to: '/home', replace: true })
+    },
 })
 
 function RouteComponent() {
