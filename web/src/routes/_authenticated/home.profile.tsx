0fa9dd489abf9fb8bbd1c82fd7cc9be3e931f67d
yohans-kasaw <emailyohans@gmail.com>
2026-01-25 23:24:21 +0300

    web: finish profile section

--- web/src/routes/_authenticated/home.profile.tsx
+++ web/src/routes/_authenticated/home.profile.tsx
@@ -3,9 +3,9 @@ import { Route as Root } from '@/routes/index'
 import { DialogHeader } from '@/components/ui/dialog'
 import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import { useState } from 'react'
-import { AtSign, CircleCheck, Mail, Phone } from 'lucide-react'
-import { Button as div } from '@/components/ui/button'
+import { useState, type ReactNode } from 'react'
+import { ChevronRight, CircleCheck, Hash, Mail, Phone } from 'lucide-react'
+import { Card, CardContent, CardTitle } from '@/components/ui/card'
 
 export const Route = createFileRoute('/_authenticated/home/profile')({
     component: RouteComponent,
@@ -37,18 +37,19 @@ function RouteComponent() {
                     navigate({ to: Root.fullPath })
                 }}
             >
-                <DialogContent>
+                <DialogContent className='max-h-[90vh] overflow-y-auto'>
                     <DialogHeader>
                         <DialogTitle>Edit profile</DialogTitle>
                     </DialogHeader>
                     <ImageGallery images={images} />
+
                     <div className="flex items-center gap-2 mt-4">
-                        <div className="text-xl sm:text-2xl">
+                        <div className="text-xl font-medium">
                             {profile.name}
                         </div>
-                        <div>{profile.age}</div>
-                        <div>
-                            <CircleCheck />
+                        <div className='text-xl font-medium'>{profile.age}</div>
+                        <div className='flex justify-center items-center w-5 h-10'>
+                            <CircleCheck size={14} className='text-blue-300'/>
                         </div>
                     </div>
 
@@ -60,65 +61,85 @@ function RouteComponent() {
                     </div>
 
                     {/* Account */}
-                    <div>
-                        <div>
-                            <div className="flex items-center gap-2">
-                                <Phone />
-                                <div className="flex flex-col justify-start items-start">
-                                    <span>{profile.phone}</span>
-                                    <span>
-                                        "tap to change your phone number"
-                                    </span>
-                                </div>
+                    <h2 className="text-muted-foreground font-medium tracking-wide">
+                        Account
+                    </h2>
+                    <Card className="p-1">
+                        <CardContent className="px-4">
+                            <div className="divide-y divide-border">
+                                <ActionItem
+                                    icon={<Phone />}
+                                    title={profile.phone}
+                                    description="Tap to change phone number"
+                                />
+                                <ActionItem
+                                    icon={<Hash />}
+                                    title={profile.handle}
+                                    description="username"
+                                />
+                                <ActionItem
+                                    icon={<Mail />}
+                                    title={profile.email}
+                                    description="Email address"
+                                />
                             </div>
-                        </div>
-                    </div>
+                        </CardContent>
+                    </Card>
 
-                    {/* Email */}
-                    <div>
-                        <div>
-                            <div className="flex items-center gap-2">
-                                <Mail/>
-                                <div className="flex flex-col justify-start items-start">
-                                    <span>{profile.email}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* handle */}
-                    <div>
-                        <div>
-                            <div className="flex items-center gap-2">
-                                <AtSign />
-                                <div className="flex flex-col justify-start items-start">
-                                    <span>{profile.handle}</span>
-                                    <span>
-                                        "tap to change your phone number"
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
+                    <h2 className="text-muted-foreground font-medium tracking-wide">
+                        Bio
+                    </h2>
                     {/* Bio */}
-                    <div>
-                        bio
-                        <div className='mt-2'>
-                            <div className="flex items-center">
-                                <Phone />
-                                <div className="flex flex-col justify-start items-start">
-                                    <span>{profile.bio}</span>
-                                </div>
+                    <Card className='p-1'>
+                        <CardContent className="pl-4">
+                            <div className="">
+                                <ActionItem
+                                    title={profile.bio}
+                                    description="Tap to change"
+                                    noChevron = {true}
+                                />
                             </div>
-                        </div>
-                    </div>
+                        </CardContent>
+                    </Card>
 
-                    {/* Bio */}
-                    <div></div>
                 </DialogContent>
             </Dialog>
+        </div>
+    )
+}
+
+function ActionItem({
+    icon,
+    title,
+    description,
+    noChevron,
+}: {
+    icon?: ReactNode
+    title: string
+    description: string
+    noChevron?: boolean
+}) {
+    return (
+        <div className="flex flex-col divide-y divide-border py-1">
+            <div className="flex w-full p-3 rounded-xl items-center gap-2 transition-colors hover:bg-accent">
+                {icon && (
+                    <div className="w-10 h-10 flex items-center justify-center">
+                        {icon}
+                    </div>
+                )}
+                <div className="flex-1 flex flex-col justify-start items-start space-y-0.5">
+                    <span className="text-[16px] font-medium leading-none">
+                        {title}
+                    </span>
+                    <span className="text-sm text-muted-foreground">
+                        {description}
+                    </span>
+                </div>
+                {
+                    !noChevron &&
+                    <ChevronRight />
+                }
+            </div>
         </div>
     )
 }
